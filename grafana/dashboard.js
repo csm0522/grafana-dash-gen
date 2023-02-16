aef7aa13fc7edee70793e087d5002025749ec625
Minya Chan <447880522@qq.com>
2023-02-16 19:44:53 +0800

    feat: add dashboard uid param

--- grafana/dashboard.js
+++ grafana/dashboard.js
@@ -36,6 +36,7 @@ function Dashboard(opts) {
 
 Dashboard.prototype._init = function _init(opts) {
     this.state = this.state;
+    this.state.uid = opts.uid || null;
     this.state.id = opts.id || null;
     this.state.title = opts.title || 'Generated Grafana Dashboard';
     this.state.originalTitle = opts.originalTitle || 'Generated Dashboard';
